5a442795f0f248aa6f8a32f0356db1a655873af1
Jorge Molina <soyjorgemolina@gmail.com>
2020-09-14 18:11:49 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var codigo = "28735983274";
+    var monto = 30.52;
+    document.getElementById("numeroCodigo").innerHTML = codigo;
+    document.getElementById("numeroMonto").innerHTML = monto;
+}
